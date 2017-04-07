a393744d0e71a80e99d7812dbab5de3ca42766cf
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-07 14:50:56 -0700

    Initial Commit

---
+++ TwoFloats/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Josiah Mory on 4/7/17.
+//  Copyright © 2017 kickinbahk Productions. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
